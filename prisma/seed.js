eb7d4d2864f1a222b77373b1fa044d7785de0db4
aomnau01~ <enzoderkord@gmail.com>
2024-02-14 00:15:44 +0700

    first commit

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,9 +4,9 @@ const prisma = new PrismaClient()
 
 const password = bcrypt.hashSync('123456')
 const userData = [
-    {username : 'aommy',  password, email: 'aommy@gmail.com'},
-    {username : 'Lo',     password, email: 'Lo123@gmail.com'},
-    {username : 'norman', password, email: 'norman@gmail.com'},
+    {firstName: 'aom', lastName:'fa', username: 'aommy',  password, email: 'aommy@gmail.com', phone:'0969495505'  },
+    {firstName: 'lo', lastName:'de', username: 'youLo',  password, email: 'Lo123@gmail.com', phone:'0957745795'  },
+    {firstName: 'nor', lastName:'as', username: 'norman', password, email: 'norman@gmail.com', phone:'0934788541' },
 ]
 
 const run = async () => {
