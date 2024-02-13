b16dfb40bccc79ee23c50b7268a83275790e418f
aomnau01~ <enzoderkord@gmail.com>
2024-02-13 18:38:11 +0700

    first commit

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,10 +1,11 @@
 const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken')
 const db = require("../models/db");
 
-module.exports.register = async (req, res, next) => {
-        const { username, password, confirmPassword, email } = req.body;
+exports.register = async (req, res, next) => {
+        const { firstName, lastname, username, password, confirmPassword, email, phone } = req.body;
         try {
-        if (!(username && password && confirmPassword)) {
+        if (!(firstName && lastname && email && phone && username && password && confirmPassword)) {
             return next(new Error('Fulfill all inputs'));
         }
         if (confirmPassword !== password) {
@@ -12,22 +13,52 @@ module.exports.register = async (req, res, next) => {
         }
         const hashedPassword = await bcrypt.hash(password, 8);
         console.log(hashedPassword);
-        const data = {
-            username,
-            password : hashedPassword,
-            email
-        };
 
-        const rs = await decodeBase64.user.create({ data })
+        const data = {
+                firstName,
+                lastname,
+                username,
+                password : hashedPassword,
+                email,
+                phone
+            };
+        
+        const rs = await db.user.create({ data })
         console.log(rs)
 
         res.json({msg: 'Register successful'})
     } catch (err) {
-        next(err)
+        next(err);
     }
 };
 
-module.exports.login = (req, res, next) => {
-    res.send('in Login...');
+exports.login = async (req, res, next) => {
+    const {username, password} = req.body
+    try {
+  
+      if( !(username.trim() && password.trim()) ) {
+        throw new Error('username or password must not blank')
+      }
+ 
+      const user = await db.user.findFirstOrThrow({ where : { username }})
 
-};
+      const pwOk = await bcrypt.compare(password, user.password)
+      if(!pwOk) {
+        throw new Error('invalid login')
+      }
+
+      const payload = { id: user.id }
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+      })
+      console.log(token)
+      res.json({token : token})
+    }catch(err) {
+      next(err)
+    }
+  };
+
+exports.getme = async (req, res, next) => { 
+      res.json(user);
+
+  };
